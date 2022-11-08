2f616d9b91d48bbb81c9be42cb91948afbbdad66
mohammed <mohammed.dbs.660@gmail.com>
2022-11-08 13:35:43 +0200

    edit homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -11,21 +11,8 @@ export default function HomePage(){
     return (
         <>
       <div className="land pt-5 ">
-        <div className="row"> 
-           <div className="col-sm-6">
-           <div className="f-holder mt-5">
-           <img className="logo" src={logo}  data-aos='flip-right' data-aos-duration='2000'/>
-           </div>
-           </div>
-           <div className="col-sm-6">
-          <div className="f-holder">
-                <h1 className="brand text-white  mt-5" data-aos='fade-up' data-aos-duration='3000'>UNITY <span>V</span>ISION</h1>
-          </div>
-          <div className="f-holder">
-                <h1 className="find text-white" data-aos='fade-right' data-aos-duration='3000' data-aos-delay='300'>Find your Passion</h1>
-          </div>
-          
-           </div>
+        
+       
         </div>
        
 
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,40 @@
 import React from 'react';
-import logo from '../images/logo.svg'
 
 export default function Logo (){
     return(
-        <img src={logo} style={{width:'60px'}} />
+        <div className='logo-compo'>
+            <svg width="50" height="50" viewBox="0 0 784 820" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="logo 1" clip-path="url(#clip0_3_82)">
+<g id="TOP-SIDE">
+<path id="Vector" d="M728.762 192.27C728.762 192.27 59.9325 275.058 9.44107 298.704L211.355 -0.0532837H539.471L728.762 192.27Z" fill="#D7D7D7"/>
+</g>
+<g id="LEFT-SIDE" filter="url(#filter0_i_3_82)">
+<path id="Vector_2" d="M3.05176e-05 316.229L369.115 254.25C369.115 254.25 244.503 614.987 211.355 810.463L0.104334 514.884L3.05176e-05 316.229Z" fill="#11484D"/>
+</g>
+<g id="BOTTOM-SIDE">
+<path id="Vector_3" d="M326.525 424.293L222.413 820H545.809L326.525 424.293Z" fill="#9FD6DF"/>
+</g>
+<g id="R-SIDE">
+<path id="Vector_4" d="M553.685 810.463L728.788 622.133L784 459.263L411.704 556.186L553.685 810.463Z" fill="#BCA1A3"/>
+</g>
+</g>
+<defs>
+<filter id="filter0_i_3_82" x="3.05176e-05" y="254.25" width="369.115" height="560.213" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="4"/>
+<feGaussianBlur stdDeviation="2"/>
+<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="shape" result="effect1_innerShadow_3_82"/>
+</filter>
+<clipPath id="clip0_3_82">
+<rect width="784" height="820" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+
+        </div>
     )
 }
